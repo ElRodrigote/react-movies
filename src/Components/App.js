9bb2a868e552502e184efd5351bd19294f625a37
Rodrigo <rodrigojali@gmail.com>
2017-10-26 17:26:55 -0300

    Now the data displays on the localserver

--- src/Components/App.js
+++ src/Components/App.js
@@ -5,12 +5,45 @@ import React, { Component } from 'react';
 //css
 require('style-loader!./App.css');
 
+
 class App extends Component {
-  render() {
-    return (
-      <h1>Movie Reviews</h1>
-    );
-  }
+
+	constructor() {
+		super();
+		this.state = {
+			movies: [],
+		}
+		
+	}
+
+	componentWillMount(){
+		fetch("http://localhost:8080/src/data.json")
+		.then((res) => res.json())
+		.then((data) => {
+			this.setState({movies: data.movies});
+		})
+		.catch(console.log)
+	}
+
+	render(){
+		let movies = this.state.movies;
+		return (
+			<div className="movie-list">
+			{
+				movies.map(movie => {
+					return(
+						<ul key={movie.id} className="movie-info">
+							<li>{movie.title}</li>
+							<li>{movie.year}</li>
+							<li>{movie.synopsis}</li>
+							<li><img src={movie['cover-url']} /></li>
+						</ul>
+					);
+				})
+			}
+			</div>
+		);
+	}
 }
 
 export default App;
