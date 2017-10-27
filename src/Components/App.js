d03453e712e3e7fb5b193613456297263286c9c6
Rodrigo <rodrigojali@gmail.com>
2017-10-27 16:54:33 -0300

    Styling added to comps

--- src/Components/App.js
+++ src/Components/App.js
@@ -25,34 +25,46 @@ class App extends Component {
 		.catch(console.log)
 	}
 
+	drawStars(arg){
+		return new Array(arg);
+	} 
+
 	render(){
 		let movies = this.state.movies;
-		let score = Math.round(this.state.movies.score * 10);
-		let stars = (arg) => new Array(arg);
 		return (
-			<div className="movie-list">
+			<ul className="movie-list">
 			{
 				movies.map(movie => {
+					let score = Math.round(movie.score*10);
 					return(
-						<div key={movie.id} className="movie-info">
-							<span>{movie.id}.</span>
-							<span>{movie.title}</span>
-							<span>({movie.year})</span>
-							{stars({score})
-								.map(star => {
-									return(
-										<i key={movie.id} className="fa fa-star fa-fw"></i>
-									);
-								})
-							}
-							
-							<div>{movie.synopsis}</div>
-							<div><img src={movie['cover-url']} /></div>
-						</div>
+						<li key={movie.id} className="movie-wrapper">
+							<div className="movie-image">
+								<a href={movie["url"]} target="_blank">
+									<img src={movie['cover-url']} />
+								</a>
+							</div>
+							<div className="movie-id">{movie.id}.</div>
+							<div className="movie-info">
+								<b>
+									<a href={movie["url"]} target="_blank">{movie.title}</a>
+									<span className="movie-info-year">({movie.year})</span>
+								</b>
+								<h3 className="movie-info-title"></h3>
+								<div className="movie-info-synopsis">{movie.synopsis} ({movie["runtime-in-minutes"]} mins.)</div>
+								<div className="movie-info-rating">{this.drawStars(score).length}</div>
+								{/*this.drawStars({score})
+									.map(star => {
+										return(
+											<span key={movie.id} className="fa fa-star fa-fw"></span>
+										);
+									})
+								*/}
+							</div>
+						</li>
 					);
 				})
 			}
-			</div>
+			</ul>
 		);
 	}
 }
